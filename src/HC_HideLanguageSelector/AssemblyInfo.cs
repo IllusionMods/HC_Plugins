b9c241ba6301058475b5076a07c949dd6d66cdfe
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-12-23 10:55:21 +0100

    Add HC_HideLanguageSelector

---
+++ src/HC_HideLanguageSelector/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using HC_HideLanguageSelector;
+
+[assembly: AssemblyTitle(HideLanguageSelector.DisplayName)]
+[assembly: AssemblyDescription(HideLanguageSelector.DisplayName)]
+[assembly: AssemblyCompany("https://github.com/IllusionMods/HC_Plugins")]
+[assembly: AssemblyProduct(HideLanguageSelector.GUID)]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+
+[assembly: AssemblyVersion(HideLanguageSelector.Version)]
---
+++ src/HC_HideLanguageSelector/HideLanguageSelector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using HC;
+using HC.Scene;
+
+namespace HC_HideLanguageSelector
+{
+    [BepInPlugin(GUID, DisplayName, Version)]
+    public class HideLanguageSelector : BasePlugin
+    {
+        public const string GUID = "HideLanguageSelector";
+        public const string DisplayName = "Hide language selection in title menu";
+        public const string Version = "1.0";
+
+        public override void Load()
+        {
+            Harmony.CreateAndPatchAll(typeof(Hooks), GUID);
+        }
+
+        public static class Hooks
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(TitleButton), "Start")]
+            private static void TitleMenu_Start(TitleButton __instance)
+            {
+                if (__instance.Event == TitleScene.Events.Language)
+                    __instance.gameObject.SetActive(false);
+            }
+        }
+    }
+}
