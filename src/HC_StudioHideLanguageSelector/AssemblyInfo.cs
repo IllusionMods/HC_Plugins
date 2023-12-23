45918faf2e162edf40246ec3dce9da4cf9fdd25a
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2023-12-23 13:35:07 +0100

    Add StudioHideLanguageSelector

---
+++ src/HC_StudioHideLanguageSelector/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using StudioHideLanguageSelector;
+
+[assembly: AssemblyTitle(StudioHideLanguageSelectorPlugin.DisplayName)]
+[assembly: AssemblyDescription(StudioHideLanguageSelectorPlugin.DisplayName)]
+[assembly: AssemblyCompany("https://github.com/IllusionMods/HC_Plugins")]
+[assembly: AssemblyProduct(StudioHideLanguageSelectorPlugin.GUID)]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+
+[assembly: AssemblyVersion(StudioHideLanguageSelectorPlugin.Version)]
---
+++ src/HC_StudioHideLanguageSelector/StudioHideLanguageSelectorPlugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using Cysharp.Threading.Tasks;
+using DigitalCraft;
+using HarmonyLib;
+
+namespace StudioHideLanguageSelector
+{
+    [BepInPlugin(GUID, DisplayName, Version)]
+    [BepInProcess("DigitalCraft")]
+    public class StudioHideLanguageSelectorPlugin : BasePlugin
+    {
+        public const string GUID = "HC_StudioHideLanguageSelector";
+        public const string DisplayName = "Hide language selection in studio title menu";
+        public const string Version = "1.0";
+
+        public override void Load()
+        {
+            Harmony.CreateAndPatchAll(typeof(Hooks), GUID);
+        }
+
+        private static class Hooks
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(StartScene), "Start")]
+            private static void StartScene_Start_Postfix(StartScene __instance, ref UniTask __result)
+            {
+                __result = __result.ContinueWith((Il2CppSystem.Action)new Action(() => __instance.buttonLanguage.gameObject.SetActive(false)));
+            }
+        }
+    }
+}
